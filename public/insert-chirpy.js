26e392e4a1d06002f9a3c58049b6a85cd2b3fe9a
Damien Erambert <damien@erambert.me>
2024-10-10 21:08:54 -0700

    blah

--- public/insert-chirpy.js
+++ public/insert-chirpy.js
@@ -1,16 +1,16 @@
-(() => {
-  if (document.querySelector("body.post > main")) {
-    document
-      .querySelector("body.post > main")
-      .insertAdjacentHTML(
-        "beforeend",
-        '<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div>'
-      );
-    const script = document.createElement("script");
-    script.setAttribute("data-chirpy-domain", "damien.zone");
-    script.setAttribute("src", "https://chirpy.dev/bootstrapper.js");
-    document
-      .querySelector("body.post > main")
-      .insertAdjacentElement("beforeend", script);
-  }
-})();
+// (() => {
+//   if (document.querySelector("body.post > main")) {
+//     document
+//       .querySelector("body.post > main")
+//       .insertAdjacentHTML(
+//         "beforeend",
+//         '<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div>'
+//       );
+//     const script = document.createElement("script");
+//     script.setAttribute("data-chirpy-domain", "damien.zone");
+//     script.setAttribute("src", "https://chirpy.dev/bootstrapper.js");
+//     document
+//       .querySelector("body.post > main")
+//       .insertAdjacentElement("beforeend", script);
+//   }
+// })();
