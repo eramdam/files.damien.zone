20cc0acfa59d07553d4c9ea49a0054e30f1ce4bb
Damien Erambert <damien@erambert.me>
2024-10-10 22:02:01 -0700

    Update insert-chirpy.js

--- public/insert-chirpy.js
+++ public/insert-chirpy.js
@@ -1,16 +1,18 @@
 (() => {
-  if (document.querySelector("body.post > main")) {
-    document
-      .querySelector("body.post > main")
-      .insertAdjacentHTML(
-        "beforeend",
-        '<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div>'
-      );
-    const script = document.createElement("script");
-    script.setAttribute("data-chirpy-domain", "damien.zone");
-    script.setAttribute("src", "https://chirpy.dev/bootstrapper.js");
-    document
-      .querySelector("body.post > main")
-      .insertAdjacentElement("beforeend", script);
+  const mainPost = document.querySelector("body.post > main");
+  // If we're not on a post page, nothing to do.
+  if (!mainPost) {
+    return;
   }
+
+  // Insert the "target" chirpy HTML snippet
+  mainPost.insertAdjacentHTML(
+    "beforeend",
+    '<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div>'
+  );
+  // Make and insert the actual chirpy bootstrapper script element
+  const script = document.createElement("script");
+  script.setAttribute("data-chirpy-domain", "damien.zone");
+  script.setAttribute("src", "https://chirpy.dev/bootstrapper.js");
+  mainPost.insertAdjacentElement("beforeend", script);
 })();
