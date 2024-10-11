53598a837c218bcd12b26265b9512c0550ec0904
Damien Erambert <damien@erambert.me>
2024-10-10 18:47:22 -0700

    Update insert-chirpy.js

--- public/insert-chirpy.js
+++ public/insert-chirpy.js
@@ -3,6 +3,6 @@
     .querySelector("body.post > main")
     .insertAdjacentHTML(
       "beforeend",
-      `<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div><script defer src="https://chirpy.dev/bootstrapper.js" data-chirpy-domain="damien.zone"></script>`
+      `<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div><script src="https://chirpy.dev/bootstrapper.js" data-chirpy-domain="damien.zone"></script>`
     );
 })();
