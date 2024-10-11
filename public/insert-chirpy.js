dd037a573c576ccea2a6541eafd4e72b221a6062
Damien Erambert <damien@erambert.me>
2024-10-10 18:49:30 -0700

    Update insert-chirpy.js

--- public/insert-chirpy.js
+++ public/insert-chirpy.js
@@ -1,8 +1,14 @@
 (() => {
-  document
-    .querySelector("body.post > main")
-    .insertAdjacentHTML(
-      "beforeend",
-      `<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div><script src="https://chirpy.dev/bootstrapper.js" data-chirpy-domain="damien.zone"></script>`
-    );
+  if (document.querySelector("body.post > main")) {
+    document
+      .querySelector("body.post > main")
+      .insertAdjacentHTML(
+        "beforeend",
+        '<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div>'
+      );
+    const script = document.createElement("script");
+    script.setAttribute("data-chirpy-domain", "damien.zone");
+    script.setAttribute("src", "https://chirpy.dev/bootstrapper.js");
+    document.querySelector("body.post > main").insertAdjacentElement(script);
+  }
 })();
