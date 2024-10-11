262ec2a9325dff2ee7a0af2bc6661882f64afad4
Damien Erambert <damien@erambert.me>
2024-10-10 18:46:26 -0700

    Create insert-chirpy.js

---
+++ public/insert-chirpy.js
@@ -0,0 +1,8 @@
+(() => {
+  document
+    .querySelector("body.post > main")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div><script defer src="https://chirpy.dev/bootstrapper.js" data-chirpy-domain="damien.zone"></script>`
+    );
+})();
