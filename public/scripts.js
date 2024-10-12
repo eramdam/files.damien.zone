4a3cf87c3d439838f38c158ce3b715d145dcd902
Damien Erambert <damien@erambert.me>
2024-10-12 00:48:04 -0700

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -5,14 +5,12 @@
     return;
   }
 
-  // Insert the "target" chirpy HTML snippet
   mainPost.insertAdjacentHTML(
     "beforeend",
-    '<div data-chirpy-theme="system" data-chirpy-comment="true" id="chirpy-comment"></div>'
+    `<comentario-comments live-update="false" no-fonts="true"></comentario-comments>`
   );
-  // Make and insert the actual chirpy bootstrapper script element
+
   const script = document.createElement("script");
-  script.setAttribute("data-chirpy-domain", "damien.zone");
-  script.setAttribute("src", "https://chirpy.dev/bootstrapper.js");
+  script.setAttribute("src", "https://comments.damien.zone/comentario.js");
   mainPost.insertAdjacentElement("beforeend", script);
 })();
